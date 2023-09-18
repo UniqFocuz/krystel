a04806940cdeb72ace75d1c3cc659537f25f5a24
UniqFocuz <it@rebornx.io>
2023-09-17 21:49:07 -0400

    WalletHash

--- src/lib/support.js
+++ src/lib/support.js
@@ -1,11 +1,11 @@
 export function copyToClipboard(text, toast, prompt) {
     navigator.clipboard.writeText(text)
       .then(() => {
-            toast({
-                title: `${prompt} copied to Clipboard!`,
-                variant: 'subtle',
-                status: 'info',
-            })
+          toast({
+              title: `${prompt} copied to Clipboard!`,
+              variant: 'subtle',
+              status: 'info',
+          })
       })
 }
 
